cc48649e8306b6f87abc5bcfc37e1cd3f358280a
Jing <jing.tang9403@gmail.com>
2026-01-30 00:00:52 -0500

    update

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// Footer year
-document.getElementById("year").textContent = new Date().getFullYear();
+const images = ["assets/letterOG.png", "assets/letterFlipped.png"];
+let index = 0;
+
+setInterval(() => {
+  document.getElementById("letter").src = images[index];
+  index = (index + 1) % images.length;
+}, 600);
 
 // Button interaction
-document.getElementById("btn").addEventListener("click", () => {
-  document.getElementById("output").textContent = "Button clicked.";
+document.getElementById("letter").addEventListener("click", () => {
+    document.getElementById("letter").style.display = "none";
+    document.getElementById("clickMe").style.display = "none";
+    document.getElementById("note").style.display = "flex";
+    
+
+
+
 });
+
+
+const btn = document.getElementById("btn");
+    const content = document.getElementById("content");
+
+    btn.addEventListener("click", () => {
+      content.classList.add("show");
+    });
