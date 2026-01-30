2395c5636ff77b0503bd5be3aaa9c59b3f0c90cc
Jing <jing.tang9403@gmail.com>
2026-01-30 15:03:19 -0500

    finished

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const images = ["assets/letterOG.png", "assets/letterFlipped.png"];
 let index = 0;
-
+let clicks = 0; 
+const dict = {
+  1: "Really?",
+  2: "Are you Sure?",
+  3: "Try again.",
+  4: "Do you hate me?",
+  5: "...",
+  6: ":(",
+  0: ":((((("
+};
 setInterval(() => {
   document.getElementById("letter").src = images[index];
   index = (index + 1) % images.length;
@@ -10,17 +19,16 @@ setInterval(() => {
 document.getElementById("letter").addEventListener("click", () => {
     document.getElementById("letter").style.display = "none";
     document.getElementById("clickMe").style.display = "none";
-    document.getElementById("note").style.display = "flex";
-    
-
-
-
+    //document.getElementById("note").style.display = "flex";
+    document.getElementById("note").classList.add("show");
+    document.getElementById("yesBtn").classList.add("show");
+    document.getElementById("noBtn").classList.add("show");
 });
 
+document.getElementById("noBtn").addEventListener("click", () => {
+    clicks += 1;
+    document.getElementById("noBtn").textContent = dict[clicks % 7];
+})
 
-const btn = document.getElementById("btn");
-    const content = document.getElementById("content");
 
-    btn.addEventListener("click", () => {
-      content.classList.add("show");
-    });
+
